8dd8d4427c007d711c096acc722677faca7f887a
Florian Jung <flo@windfis.ch>
2025-06-22 17:56:08 +0200

    timing

--- main.py
+++ main.py
@@ -5,16 +5,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as ss
 from numpy import log, exp
+import time
+
+print_orig = print
+last_timestamp = None
+def print(*args, **kwargs):
+	global last_timestamp
+	if last_timestamp is None: last_timestamp = time.time()
+	diff = time.time() - last_timestamp
+	last_timestamp = time.time()
+	print_orig(f'[+{diff*1000:7.2f}ms]', *args, **kwargs)
 
 def cfar_kernel(n, guard):
-	#return np.array( [-0.5/n]*n + [0]*guard + [1] + [0]*guard + [-0.5/n]*n )
 	return np.array( [0]*n + [0]*guard + [1] + [0]*guard + [-1/n]*n )
-	#return np.array( [-1/n]*n + [0]*guard + [1] + [0]*guard + [0]*n )
+
+def find_local_maxima(a):
+	return (np.convolve(a, [1,-1])[:-1]>=0) & (np.convolve(a, [1,-1])[1:]<=0)
+
+def log_avg(a, bins):
+	l = log(a.shape[1]) / bins
+	bins = int(bins)
+	result=np.zeros((a.shape[0], bins))
+
+	for i in range(bins):
+		begin = int(exp(i*l))
+		end = int(exp((i+1)*l))
+		if end <= begin: end=begin+1
+
+		result[:, i] = np.sum(a[:, begin:end], axis=1) / (end-begin)
+	
+	return result
+
+def log_avg2(a, bins):
+	l = log(a.shape[1]) / bins
+	bins = int(bins)
+	result=np.zeros((a.shape[0], bins))
+
+	for i in range(bins):
+		begin = int(exp(i*l))
+		end = int(exp((i+1)*l))
+		if end <= begin: end=begin+1
+
+		result[:, i] = np.log( np.sum( np.exp(a[:, begin:end]), axis=1) / (end-begin) )
+	
+	return result
+
+def overlapping_windows(data, window, step):
+	N = len(window)
+	step=int(step)
+	print(N, step, data.strides)
+	print(len(data), len(window))
+	return np.lib.stride_tricks.as_strided(data,( (len(data)-len(window)) // step ,N), [data.strides[0]*step, data.strides[0]]) * window
 
 
 print("reading file")
-#data, samplerate = sf.read("vimana.mp3")
-#t0=100
+
 data, samplerate = sf.read(sys.argv[1])
 t0=int(sys.argv[2])
 
@@ -34,26 +79,23 @@ print(data.shape)
 
 print("trimming")
 data = data[t0*samplerate: (t0+5)*samplerate]
-#data = data[120*samplerate: 130*samplerate]
 
 timestep_desired = 1/1000
 samplestep = int(samplerate * timestep_desired)
 timestep_real = samplestep / samplerate
 
 print(f"using a timestep of {timestep_real*1000:.3f}ms")
-
-def overlapping_windows(data, window, step):
-	N = len(window)
-	step=int(step)
-	print(N, step, data.strides)
-	print(len(data), len(window))
-	return np.lib.stride_tricks.as_strided(data,( (len(data)-len(window)) // step ,N), [data.strides[0]*step, data.strides[0]]) * window
+print("windowing")
 
 x = overlapping_windows(data, np.hamming(samplerate/20), samplestep)
 
+print("fft")
 y = np.absolute( np.fft.rfft(x, axis=1) )
 
 y = np.log10(y + 1e-4) * 20
+
+
+
 
 #y = ss.convolve2d(y, np.transpose([cfar_kernel( int(samplerate*0.01), int(samplerate*0.01) )]))
 print("convolving")
@@ -62,40 +104,10 @@ print("convolving")
 my_kernel = cfar_kernel( int(0.2 / timestep_real), int(0.04 / timestep_real) )
 y = np.apply_along_axis( lambda foo: ss.oaconvolve(foo, my_kernel, 'valid'), axis=0, arr=y )
 
+print("maximum")
 y = np.fmax(y - 10, 0)
 
-
-def find_local_maxima(a):
-	return (np.convolve(a, [1,-1])[:-1]>=0) & (np.convolve(a, [1,-1])[1:]<=0)
-
-def log_avg(a, bins):
-	l = log(a.shape[1]) / bins
-	bins = int(bins)
-	result=np.zeros((a.shape[0], bins))
-
-	for i in range(bins):
-		begin = int(exp(i*l))
-		end = int(exp((i+1)*l))
-		if end <= begin: end=begin+1
-
-		result[:, i] = np.sum(a[:, begin:end], axis=1) / (end-begin)
-	
-	return result
-		
-def log_avg2(a, bins):
-	l = log(a.shape[1]) / bins
-	bins = int(bins)
-	result=np.zeros((a.shape[0], bins))
-
-	for i in range(bins):
-		begin = int(exp(i*l))
-		end = int(exp((i+1)*l))
-		if end <= begin: end=begin+1
-
-		result[:, i] = np.log( np.sum( np.exp(a[:, begin:end]), axis=1) / (end-begin) )
-	
-	return result
-		
+print("binning")
 bins = math.log(y.shape[1], 2)*12
 y = log_avg2(y, bins)
 

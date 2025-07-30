5cf47b6dbfdb3c63118dfd01c804cec54093097b
Florian Jung <flo@windfis.ch>
2025-07-30 19:29:00 +0200

    add total sum to time tracker

--- timetracker.py
+++ timetracker.py
@@ -25,3 +25,7 @@ class TimeTracker:
 		print(f"{'':50s}  {'total':>9s} ={'number':>8s} x {'average':>10s} ± {'stddev':>10s}")
 		for key in self.ordered_keys:
 			print(f"{key:50s}: {np.sum(self.times[key]):8.2f}s ={len(self.times[key]):8d} x {1000*np.average(self.times[key]):8.1f}ms ± {1000*np.std(self.times[key]):8.1f}ms")
+
+		total = sum(sum(self.times.values(), start=[]))
+		print(f"{'TOTAL':50s}: {total:8.2f}s")
+
